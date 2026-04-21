bfc0bec3128317d6512b6ab244c98401a49b566b
PavelPonomarenko2003 <94495275+PavelPonomarenko2003@users.noreply.github.com>
2026-04-21 19:49:10 +0300

    Update ErrorResponseDTO.java

--- src/main/java/com/example/dto/ErrorResponseDTO.java
+++ src/main/java/com/example/dto/ErrorResponseDTO.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Schema(description = "Standard API error response structure")
